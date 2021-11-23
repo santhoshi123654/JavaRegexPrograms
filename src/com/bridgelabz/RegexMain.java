d04a17c97fdf2f2bc7c8bd58cbb76dd23a54e863
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-23 14:05:21 +0530

    Added:Password Rule1 Pattern

---
+++ src/com/bridgelabz/RegexMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class RegexMain {
+	
+	public static void main(String[] args) {
+	
+		String pattern = "^([a-z]){8,}$";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Password :");
+		String passwordRule1 = sc.nextLine();
+		System.out.println(Pattern.matches(pattern, passwordRule1));
+	
+	}
+}
