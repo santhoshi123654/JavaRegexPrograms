39a3c3a11b032a9ea72226dd387717d21cc0d6d9
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-23 13:06:52 +0530

    Added:Modified Code

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RegexMain {
 	public static void main(String[] args) {
 	
-	String pattern = "^([A-Za-z]){3,}$";
+	String pattern = "^([A-Z]{1})([a-z]{2,})$";
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter the First Name :");
 	String fName = sc.nextLine();
