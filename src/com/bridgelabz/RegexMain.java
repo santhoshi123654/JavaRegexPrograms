1f095f84f2e4b8ec6973e06fcbee79858a811544
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 15:40:23 +0530

    Added:EmailId Pattern

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -6,13 +6,10 @@ public class RegexMain {
 	
 	public static void main(String[] args) {
 	
-	String pattern = "^([a-z]){3}[.][(a-z)]{2,}+@[(a-z)]{2}[.][(a-z)]{2}[.][(a-z)]{2,}$";
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the Email Id :");
-	String emailId1 = sc.nextLine();
-	System.out.println(Pattern.matches(pattern, emailId1));
-	String emailId = "abc.xyz@bl.co.in";
-	System.out.println(Pattern.matches(pattern, emailId));
+		PatternCheck patterncheck = new PatternCheck();
+		patterncheck.firstName();
+		patterncheck.lastName();
+		patterncheck.emailIdPattern();
 	
 	}
 }
