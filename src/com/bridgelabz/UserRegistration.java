92c455b98b4a5c37dbc6a191fd3508c25be63550
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 15:22:54 +0530

    Added: User Registration Details

---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,42 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+	String firstName;
+	String LastName;
+	String phoneNumber;
+	String emailId;
+	String password;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
+   
