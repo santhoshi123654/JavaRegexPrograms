376ebdccdca498ac7bdb9ac360b2e0c4eaac1d21
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 14:35:59 +0530

    Added:User FirstName Pattern

---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,49 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+	String firstName;
+	String LastName;
+	String phoneNumber;
+	String emailId;
+	String password;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserRegistration [firstName=" + firstName + ", LastName=" + LastName + ", phoneNumber=" + phoneNumber
+				+ ", emailId=" + emailId + ", password=" + password + "]";
+	}
+}
+
