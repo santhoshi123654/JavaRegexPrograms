77f53c824251415e8186c5752c594f035ce8e26e
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 15:22:02 +0530

    Added:LastName Pattern

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -4,14 +4,11 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class RegexMain {
-	
 	public static void main(String[] args) {
-	
-	String pattern = "^([A-Z]){1}([a-z]){2,}$";
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the Last Name :");
-	String lName = sc.nextLine();
-	System.out.println(Pattern.matches(pattern, lName));
+
+		PatternCheck patterncheck = new PatternCheck();
+		patterncheck.firstName();
+		patterncheck.lastName();
 	
 	}
 }
