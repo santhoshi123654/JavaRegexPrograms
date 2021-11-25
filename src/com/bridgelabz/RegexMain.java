25dfcc4e2050afa11764e33c2140bd1e23a05fef
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 16:56:03 +0530

    Added:PasswordRule2 Pattern

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -1,16 +1,18 @@
 package com.bridgelabz;
+
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class RegexMain {
-	
 	public static void main(String[] args) {
-	
-	String pattern = "^(?=.*[a-z])(?=.*[A-Z]).{8,}$";
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the Password :");
-	String passwordRule2 = sc.nextLine();
-	System.out.println(Pattern.matches(pattern, passwordRule2));
-	
+
+		PatternCheck patterncheck = new PatternCheck();
+		patterncheck.firstName();
+		patterncheck.lastName();
+		patterncheck.emailIdPattern();
+		patterncheck.phoneNumberPattern();
+		patterncheck.passwordPatternRule1();
+		patterncheck.passwordPatternRule2();
+
 	}
 }
