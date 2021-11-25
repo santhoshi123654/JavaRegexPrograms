815410b5d0976cabdb8577886b5368b568d67103
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 17:04:38 +0530

    Added:PasswordRule3 Pattern

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -1,15 +1,18 @@
 package com.bridgelabz;
+
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class RegexMain {
-	
 	public static void main(String[] args) {
-	
-	String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the Password :");
-	String passwordRule3 = sc.nextLine();
-	System.out.println(Pattern.matches(pattern, passwordRule3));
+
+		PatternCheck patterncheck = new PatternCheck();
+		patterncheck.firstName();
+		patterncheck.lastName();
+		patterncheck.emailIdPattern();
+		patterncheck.phoneNumberPattern();
+		patterncheck.passwordPatternRule1();
+		patterncheck.passwordPatternRule2();
+		patterncheck.passwordPatternRule3();
 	}
 }
