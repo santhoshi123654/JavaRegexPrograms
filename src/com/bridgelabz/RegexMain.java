17638a8593627a1d9a7bcbea7e66f24107116676
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-25 16:07:28 +0530

    Added:Phonenumber Pattern

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -1,18 +1,15 @@
 package com.bridgelabz;
+
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class RegexMain {
-	
 	public static void main(String[] args) {
-	
-	String pattern = "^([1-9]){2}\s[(0-9)]{10}$";
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the PhoneNumber :");
-	String phoneNumber = sc.nextLine();
-	System.out.println(Pattern.matches(pattern, phoneNumber));
-	String mobileNum = "91 9919819801";
-	System.out.println(Pattern.matches(pattern, mobileNum));
-	
+
+		PatternCheck patterncheck = new PatternCheck();
+		patterncheck.firstName();
+		patterncheck.lastName();
+		patterncheck.emailIdPattern();
+		patterncheck.phoneNumberPattern();
 	}
 }
