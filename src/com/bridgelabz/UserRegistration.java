cbdf2fc88ca1b940700c0084ea97547f37867113
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-27 08:44:21 +0530

    Added: Regex Pattern for Firstname,lastname,email,phonenumber,password.

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,56 +1,55 @@
 package com.bridgelabz;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	
-	String firstName;
-	String LastName;
-	String phoneNumber;
-	String emailId;
-	String password;
-
-	public String getFirstName() {
-		return firstName;
+	public String pattern;
+	public boolean userFirstName(String firstName) {
+		pattern = "^[A-Z]{1}[a-z]{2,}";
+		System.out.println(Pattern.matches(pattern, firstName));
+		return true;
 	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	
+	public boolean userLastName(String lastName) {
+		pattern = "^[A-Z]{1}[a-z]{2,}";
+		System.out.println(Pattern.matches(pattern, lastName));
+		return true;
 	}
-
-	public String getLastName() {
-		return LastName;
+	
+	public boolean userEmailId(String emailId) {
+		pattern = "^[a-zA-Z0-9]+([-_+.a-zA-Z0-9])*@[A-Za-z]+.[A-Za-z]+(.[A-Za-z]{2})*$";
+		System.out.println(Pattern.matches(pattern, emailId));
+		return true;
 	}
-
-	public void setLastName(String lastName) {
-		LastName = lastName;
+	
+	public boolean userPhoneNumber(String phoneNumber) {
+		pattern = "^[1-9]{2}[\\s][0-9]{10}$"; 
+		System.out.println(Pattern.matches(pattern, phoneNumber));
+		return true;
 	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
+	
+	public boolean userPasswordRule1(String password) {
+		pattern = "^[a-z]{8,}";
+		System.out.println(Pattern.matches(pattern, password));
+		return true;
 	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	
+	public boolean userPasswordRule2(String password) {
+		pattern = "^(?=.*[A-Z])[a-z]{8,}$";
+		System.out.println(Pattern.matches(pattern, password));
+		return true;
 	}
-
-	public String geteMail() {
-		return emailId;
+	
+	public boolean userPasswordRule3(String password) {
+		pattern = "^(?=.*[A-Z])(?=.*[0-9])[a-z]{8,}$";
+		System.out.println(Pattern.matches(pattern, password));
+		return true;
 	}
-
-	public void seteMail(String email) {
-		this.emailId = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-	public String toString() {
-		return "UserRegistration [firstName=" + firstName + ", LastName=" + LastName + ", phoneNumber=" + phoneNumber
-				+ ", emailId=" + emailId + ", password=" + password + "]";
+	
+	public boolean userPasswordRule4(String password) {
+		pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$";
+		System.out.println(Pattern.matches(pattern, password));
+		return true;
 	}
 }
