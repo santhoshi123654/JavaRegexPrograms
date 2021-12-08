91dd3a03b7a16472c12c1441069d15df00bfda6f
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-08 11:23:19 +0530

    Added:UserRegistration Using jUnit Testing

---
+++ src/com/lamdaExpressions/UserRegistrationTest.java
@@ -0,0 +1,196 @@
+package com.lamdaExpressions;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bridgelabz.InvalidUserRegistrationException;
+import com.bridgelabz.UserRegistration;
+
+public class UserRegistrationTest {
+	
+	static UserRegistration userRegistration;
+	@BeforeClass
+	public static void init() {
+		userRegistration = new UserRegistration();
+	}
+	@Test
+	public void ifUserRegistration_FirstName_StartsWithUpperCase_Then_itis_True(){
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userFirstName("Santhoshi");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+			
+		}
+		assertEquals(true,actualResult);
+	}
+	@Test
+	public void ifUserRegistration_FirstName_if_it_doesnot_StartsWithUpperCase_Then_itis_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userFirstName("santhu");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+			
+		}
+		assertEquals(false,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_LastName_StartsWithUpperCase_Then_itis_True() {
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userLastName("Badam");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+			
+		}
+		assertEquals(true, actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_LastName_StartsWithUpperCase_Then_itis_False() {
+		boolean actualResult=false;
+		try {
+			actualResult = userRegistration.userLastName("badam");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_Emailid_pattern_is_True(){
+		boolean actualResult= true;
+		try {
+			actualResult = userRegistration.userEmailId("abc.xyz@bridgelabz.co.in");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false, actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_Emailid_pattern_is_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userEmailId("abc123@.com");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PhoneNumber_pattern_should_start_with_code_is_True() {
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userPhoneNumber("91 9987654321");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PhoneNumber_pattern_should_start_with_code_if_not_itis_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userPhoneNumber("9987654321");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule1_pattern_is_True() {
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userPasswordRule1("santhoshi");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule1_pattern_is_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userPasswordRule1("santhu");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule2_pattern_is_True() {
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userPasswordRule2("Santhoshi");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule2_pattern_is_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userPasswordRule2("santhoshi");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+	@Test
+	public void ifUserRegistration_PasswordRule3_pattern_is_True() {
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userPasswordRule3("Santhoshi1");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule3_pattern_is_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userPasswordRule3("Santhoshi");
+		} catch (InvalidUserRegistrationException e) {
+			
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule4_pattern_is_True() {
+		boolean actualResult = true;
+		try {
+			actualResult = userRegistration.userPasswordRule4("Santhu@12");
+		} catch (InvalidUserRegistrationException e) {
+
+			e.printStackTrace();
+		}
+		assertEquals(true,actualResult);
+	}
+	
+	@Test
+	public void ifUserRegistration_PasswordRule4_pattern_is_False() {
+		boolean actualResult = false;
+		try {
+			actualResult = userRegistration.userPasswordRule4("Santhu12");
+		} catch (InvalidUserRegistrationException e) {
+			e.printStackTrace();
+		}
+		assertEquals(false,actualResult);
+	}
+}
